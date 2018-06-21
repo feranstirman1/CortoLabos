45f87c3ee27ab2b22046d3f3c52191b755e54efa
feranstirman1 <00024117@uca.edu.sv>
2018-06-21 07:35:38 -0600

    Se termino la interfaz de los metodos

--- src/interfaces/metodos.java
+++ src/interfaces/metodos.java
@@ -5,10 +5,17 @@
  */
 package interfaces;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author LN710Q
  */
-public interface metodos {
+public interface metodos<Generic> {
+    public boolean create(Generic g);
+    public boolean delete(Object key);
+    public boolean update(Generic c);
     
+    public Generic read(Object key);
+    public ArrayList<Generic> readAll();
 }
