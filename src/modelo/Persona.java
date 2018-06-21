95e25b132ddaabbaca39d7b6039a7955677d468e
feranstirman1 <00024117@uca.edu.sv>
2018-06-21 07:32:19 -0600

    Se termino la clase Persona

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -10,5 +10,80 @@ package modelo;
  * @author LN710Q
  */
 public class Persona {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String profesion;
+    private boolean estado;
+    
+    public Persona(){}
+
+    public Persona(int id, String nombre, String apellido, int edad, String profesion, boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.profesion = profesion;
+        this.estado = estado;
+    }
+
+    public Persona(String nombre, String apellido, int edad, String profesion, boolean estado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.profesion = profesion;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
     
 }
